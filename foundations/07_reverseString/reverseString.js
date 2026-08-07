ebb28615fdfeadd7faac6b2d1dd8873283883925
Thapelo <tmatabane952@gmail.com>
2026-08-07 20:50:10 +0200

    completed the reverse string exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+function reverseString(str){
+    reverse = "";
+    for (let i = str.length - 1; i >= 0; i--){
+        reverse = reverse + str.charAt(i);
+    }
+    return reverse;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
