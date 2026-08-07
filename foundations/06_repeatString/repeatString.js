b05a2d5c269a322fc6fec30c22b7706c45bad597
Thapelo <tmatabane952@gmail.com>
2026-08-07 20:40:08 +0200

    completed the repeatString exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+function repeatString(str,num){
+    let result = "";
+    if (num < 0){
+        return "ERROR";
+    }
+    for (let i = 0; i < num;i++){
+        result = result + str; 
+    }
+    return result;
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
