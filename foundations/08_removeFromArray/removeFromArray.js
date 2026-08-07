b584e6fb0fc41894a49fa6cc3ec5731b2fc7d370
Thapelo <tmatabane952@gmail.com>
2026-08-07 21:36:33 +0200

    completed the remove from array exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-};
+function removeFromArray(...args){
+    const len = args.length;
+    let arr = args[0];
+    for (let i = 1; i < len; i++){
+        arr= arr.filter(item => item !== args[i]);
+    }
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
