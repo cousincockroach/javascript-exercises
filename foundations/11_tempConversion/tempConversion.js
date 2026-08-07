7aca83076a0c50164675de5df659f714a5c40dbd
Thapelo <tmatabane952@gmail.com>
2026-08-07 22:52:06 +0200

    completed conversion exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
-};
+function convertToCelsius(farenheit){
+  celsius = (farenheit-32) * 5/9;
+  return Number(celsius.toFixed(1));
+}
 
-const convertToFahrenheit = function() {
-};
+function convertToFahrenheit(celcius){
+  farenheit = (celcius * 9/5) + 32;
+  return Number(farenheit.toFixed(1))
+}
 
 // Do not edit below this line
 module.exports = {
